fe7eac469b33c8dbbb00e1ad6cf24712cfd54034
JudeMichaelArriba <arribajudemichael@gmail.com>
2026-02-26 03:20:14 +0800

    Use PORT env, bind 0.0.0.0; add gunicorn
    Read PORT from the environment and bind the Flask app to 0.0.0.0 (defaulting to 5000) to support container/cloud deployments. Added os import required for this change. Also added gunicorn to requirements to support running the app with a production WSGI server.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
 
 
 # Note: This python is not fully developed, some timestamp features are not included, 
@@ -66,4 +67,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
